ca57cf6361f66218a9c15ae93bfbc77532f6b853
Hieu <tranhieu14310@gmail.com>
2021-07-19 22:37:55 +0700

    refactor :recycle:

--- leetcode/String/validParentheses.ts
+++ leetcode/String/validParentheses.ts
@@ -1,24 +1,24 @@
 /**
  * https://leetcode.com/problems/valid-parentheses/
  * Ý tưởng: Push vô rồi pop ra
- * 
+ *
  */
 function isValid(s: string): boolean {
-    const objSymmetry = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    }
-    const stack = [];
-    const startS = ['(', '{', '['];
-    for (const char of s) {
-        if(startS.includes(char)){
-           stack.push(objSymmetry[char]); 
-        }else if(stack.pop() !== char){
-            return false;
-        }
-    }
-    if(stack.length === 0) return true;
-    return false;
-};
-console.log(isValid("{[]}"));
+	const objSymmetry: { [key: string]: string } = {
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	const stack = []
+	const startS = ['(', '{', '[']
+	for (const char of s) {
+		if (startS.includes(char)) {
+			stack.push(objSymmetry[char])
+		} else if (stack.pop() !== char) {
+			return false
+		}
+	}
+	if (stack.length === 0) return true
+	return false
+}
+console.log(isValid('{[]}'))
