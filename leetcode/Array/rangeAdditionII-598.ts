1883b84d92d32e8ca89c93f67743804b123b2b95
Hieu <tranhieu14310@gmail.com>
2021-07-20 13:17:15 +0700

    bad 598 :poop:

---
+++ leetcode/Array/rangeAdditionII-598.ts
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/range-addition-ii/
+ *
+ */
+function maxCount(m: number, n: number, ops: number[][]): number {
+	if (ops.length === 0) return m * n
+	const matrix: number[][] = []
+	for (let k = 0; k < m; k++) {
+		matrix[k] = []
+		for (let o = 0; o < n; o++) {
+			matrix[k][o] = 0
+		}
+	}
+	let [count, max]: number[] = [0, 0]
+	for (let z = 0; z < ops.length; z++) {
+		for (let i = 0; i < ops[z][0]; i++) {
+			for (let j = 0; j < ops[z][1]; j++) {
+				matrix[i][j]++
+				if (matrix[i][j] > max) {
+					max = matrix[i][j]
+					count = 0
+				}
+				if (matrix[i][j] === max) count++
+			}
+		}
+	}
+	return count
+}
+console.log(maxCount(3, 3, [[2, 2]]))
