500d5871eff04e1c64eda99046e5548837a60cec
Hieu <tranhieu14310@gmail.com>
2021-07-23 11:16:29 +0700

    done 169 :white_check_mark:

---
+++ leetcode/Array/majorityElement-169.ts
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/majority-element/
+ */
+function majorityElement(nums: number[]): number {
+	const hmNums: { [key: number]: number } = {}
+	for (let i = 0; i < nums.length; i++) {
+		const element = nums[i]
+		if (element in hmNums) {
+			hmNums[element]++
+		} else hmNums[element] = 1
+	}
+	const appears = nums.length / 2
+	let result: number = 0
+	Object.keys(hmNums)
+		.map((i) => parseInt(i))
+		.forEach((key) => {
+			if (hmNums[key] > appears) {
+				result = key
+			}
+		})
+	return result
+}
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]))
