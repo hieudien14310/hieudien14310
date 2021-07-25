a81598242668ad9188f9e09ae07a8ca93970b296
Hieu <tranhieu14310@gmail.com>
2021-07-25 22:21:38 +0700

    update library :heavy_plus_sign:

---
+++ Library/sortValueObject.ts
@@ -0,0 +1,27 @@
+/**
+ * Sắp xếp tăng dần (giảm dần) theo giá trị của thuộc tính (key) theo giá trị (value)
+ * Điều kiện: key của object phải là string. Nếu là number thì ko thể sort được.
+ * Cách làm: Chuyển object về thành 1 array 2 chiều. Rồi dựa vào phần tử có index là 1 trong mỗi phần tử của array con để sắp xếp.
+ */
+function sortValueOfObject(
+	list: { [key: string]: number } = {},
+	ascending: boolean = false,
+): any {
+	const toArray: any[] = Object.entries(list)
+	toArray.sort((a, b) => {
+		if (ascending) {
+			return a[1] - b[1]
+		} else {
+			return b[1] - a[1]
+		}
+	})
+	return Object.fromEntries(toArray)
+}
+let list = {
+	conac: 100,
+	memay: 75,
+	dume: 116,
+	cailon: 15,
+}
+list = sortValueOfObject(list, true)
+console.log(list)
