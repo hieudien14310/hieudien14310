5d37e7011e7e0e3b86fed5a61fdab17abb59ac40
Hieu <tranhieu14310@gmail.com>
2021-07-28 09:28:15 +0700

    done 1122, got 76ms :white_check_mark:

---
+++ leetcode/Array/relativeSortArray-1122.ts
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/relative-sort-array/
+ *
+ */
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+	const data: number[] = []
+	const hmArr1: { [key: number]: number } = {}
+	const notAppear = []
+	arr1.sort((a, b) => a - b)
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr2.indexOf(arr1[i]) >= 0) {
+			if (arr1[i] in hmArr1 && arr2.indexOf(arr1[i]) >= 0) hmArr1[arr1[i]]++
+			else {
+				hmArr1[arr1[i]] = 1
+			}
+		} else notAppear.push(arr1[i])
+	}
+	for (let j = 0; j < arr2.length; j++) {
+		const element = arr2[j]
+		while (hmArr1[element]) {
+			data.push(element)
+			hmArr1[element]--
+		}
+	}
+	return [...data, ...notAppear]
+}
+console.log(relativeSortArray([-4, 3, 2, 1, 1, 9, 6, 7, 4], [1, 2, 3, 4]))
