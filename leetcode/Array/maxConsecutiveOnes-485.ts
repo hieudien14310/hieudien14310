13d30d31ca6df0e7987afaf94724890452efc440
Hieu <tranhieu14310@gmail.com>
2021-07-28 21:08:37 +0700

    update :bulb:

--- leetcode/Array/maxConsecutiveOnes-485.ts
+++ leetcode/Array/maxConsecutiveOnes-485.ts
@@ -3,14 +3,13 @@
  * Tìm độ dài liên tiếp của số 1 dài nhất trong mảng.
  */
 function findMaxConsecutiveOnes(nums: number[]): number {
-	let count = 0,
-		maxCount = 0
+	let count: number = 0,
+		ans: number = 0
 	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] === 0) {
-			maxCount = Math.max(maxCount, count)
-			count = 0
-		} else count++
+		if (nums[i] === 1) count++
+		else count = 0
+		ans = Math.max(count, ans)
 	}
-	return Math.max(maxCount, count)
+	return ans
 }
 console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 1, 0]))
