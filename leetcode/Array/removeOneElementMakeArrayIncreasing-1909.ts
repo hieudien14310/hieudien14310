d09aa699d5c712c2a217fdf4561f4a9fb210e79f
Hieu <tranhieu14310@gmail.com>
2021-07-29 14:00:38 +0700

    add comments :bulb:

--- leetcode/Array/removeOneElementMakeArrayIncreasing-1909.ts
+++ leetcode/Array/removeOneElementMakeArrayIncreasing-1909.ts
@@ -1,6 +1,8 @@
 /**
  * https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
- *
+ * Loại bỏ 1 giá trị trong mảng để cho mảng đó là mảng có các giá trị tăng dần.
+ * Ý tưởng: Cứ loại bỏ tuần tự 1 phần tử trong mảng ban đầu (từ trái qua phải).
+ * Rồi kiểm tra mảng sau khi cắt có phải là các số tăng dần hay không.
  */
 function canBeIncreasing(nums: number[]): boolean {
 	for (let i = 0; i < nums.length; i++) {
