a369d4510fb2f74768435c15f6c6d851678db214
Hieu <tranhieu14310@gmail.com>
2021-07-29 17:47:37 +0700

    add comments :bulb:

--- leetcode/Array/distributeCandies-575.ts
+++ leetcode/Array/distributeCandies-575.ts
@@ -1,6 +1,9 @@
 /**
  * https://leetcode.com/problems/distribute-candies/
- *
+ * Tìm tổng số loại kẹo mà Alice có thể ăn được theo lời bác sĩ.
+ * Ý tưởng: Alice chỉ được ăn 1/2 số kẹo đang có.
+ * Tìm số loại kẹo mà Alice có.
+ * Sau đó so sánh với số kẹo mà Alice có thể ăn được.
  */
 function distributeCandies(candyType: number[]): number {
 	const typesCandy: Set<number> = new Set(candyType)
