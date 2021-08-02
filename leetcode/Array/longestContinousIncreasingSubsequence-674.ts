b3003a4f21a713fae4eeb94fe3ca0ca7c410f66a
Hieu <tranhieu14310@gmail.com>
2021-08-02 13:37:57 +0700

    done 674, got 76ms :white_check_mark:

---
+++ leetcode/Array/longestContinousIncreasingSubsequence-674.ts
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ *
+ */
+function findLengthOfLCIS(nums: number[]): number {
+	let [longest, j]: number[] = [0, 0]
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] < nums[i + 1]) continue
+		else {
+			longest = Math.max(longest, i - j)
+			j = i + 1
+		}
+	}
+	return longest + 1
+}
+console.log(findLengthOfLCIS([1, 3, 5, 7, 5, 6, 7, 8, 9, 10]))
