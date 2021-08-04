6a45f4c849026f96ff407acc3a44f0d1ecf43bdc
Hieu <tranhieu14310@gmail.com>
2021-08-04 11:02:15 +0700

    done 997, got 108ms :white_check_mark:

---
+++ leetcode/Array/findTownJudge-997.ts
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/find-the-town-judge/
+ * Tìm 1 con số trong các cặp số thỏa mãn điều kiện sau:
+ *  - 1 con số không tin tưởng bất kỳ con số nào cả.
+ *  - Tất cả các con số điều tin tưởng con số ở đk 1
+ *  - Chỉ có duy nhất 1 con số hợp 2 đk trên.
+ */
+function findJudge(n: number, trust: number[][]): number {
+	if (n === 1) return 1
+	const setTrusts: Set<number> = new Set()
+	const hmJudge: { [key: number]: number } = {}
+	for (let i = 0; i < trust.length; i++) {
+		setTrusts.add(trust[i][0])
+		if (trust[i][1] in hmJudge) hmJudge[trust[i][1]]++
+		else hmJudge[trust[i][1]] = 1
+	}
+	let judge: number = -1
+	Object.keys(hmJudge).forEach((key) => {
+		if (hmJudge[parseInt(key)] == n - 1 && !setTrusts.has(parseInt(key))) {
+			judge = parseInt(key)
+		}
+	})
+	return judge
+}
+console.log(findJudge(2, [[1, 2]]))
