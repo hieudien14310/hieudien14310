840fe8aafbb6cedd564479ad66e91d08bf61feed
Hieu <tranhieu14310@gmail.com>
2021-08-04 20:46:59 +0700

    done 1287, got 92ms

---
+++ leetcode/Array/elementAppearingMoreThan25InArray-1287.ts
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+ *
+ */
+function findSpecialInteger(arr: number[]): number {
+	const quarter: number = arr.length / 4
+	const hmFreq: { [key: number]: number } = {}
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i]
+		if (element in hmFreq) hmFreq[element]++
+		else hmFreq[element] = 1
+	}
+	const keys: any[] = Object.keys(hmFreq)
+	for (let j = 0; j < keys.length; j++) {
+		const key = keys[j]
+		if (hmFreq[key] > quarter) return key
+	}
+	return 1
+}
+console.log(findSpecialInteger([1, 1]))
