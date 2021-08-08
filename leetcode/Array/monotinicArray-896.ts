fa9c3139b595e6f40c48ca87266e31723adc04ff
Hieu <tranhieu14310@gmail.com>
2021-08-08 20:21:39 +0700

    done 896, got 100ms

---
+++ leetcode/Array/monotinicArray-896.ts
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/monotonic-array/
+ *
+ */
+function isMonotonic(nums: number[]): boolean {
+	let increasing = 0,
+		descrasing = 0
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] === nums[i + 1]) continue
+		if (nums[i] < nums[i + 1]) {
+			increasing++
+		} else descrasing++
+	}
+	if (increasing !== 0 && descrasing !== 0) return false
+	return true
+}
+console.log(isMonotonic([6, 5, 4, 4]))
