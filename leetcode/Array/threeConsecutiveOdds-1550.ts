37dbdb85fb246d4fcff8962d499597788a181192
Hieu <tranhieu14310@gmail.com>
2021-08-11 23:29:57 +0700

    done 1550, got 84ms :white_check_mark:

---
+++ leetcode/Array/threeConsecutiveOdds-1550.ts
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/three-consecutive-odds/
+ *
+ */
+function threeConsecutiveOdds(arr: number[]): boolean {
+	let i = 0,
+		count = 0
+	for (let j = 0; j < arr.length; j++) {
+		if (arr[j] % 2 !== 0) {
+			i++
+			count = Math.max(count, i)
+		} else i = 0
+	}
+	if (count >= 3) return true
+	return false
+}
+console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]))
