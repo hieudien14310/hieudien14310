468e98b5f29f89d1b3a9c0c704ca2587db8de7b9
Hieu <tranhieu14310@gmail.com>
2021-08-13 16:26:43 +0700

    done 128, got 129ms :white_check_mark:

---
+++ leetcode/Array/longestConsecutiveSequence-128.ts
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * Tìm độ dài lớn nhất của chuỗi liên tiếp.
+ */
+function longestConsecutive(nums: number[]): number {
+	nums.sort((a, b) => a - b)
+	let count = -1,
+		consec = 0
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] + 1 === nums[i + 1]) consec++
+		else if (nums[i] === nums[i + 1]) continue
+		else {
+			count = Math.max(count, consec)
+			consec = 0
+		}
+	}
+	return count + 1
+}
+console.log(longestConsecutive([1, 2, 0, 1]))
