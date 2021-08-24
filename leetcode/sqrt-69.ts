7dec405311ef894b8f98092e6d62668b92759496
Hieu <tranhieu14310@gmail.com>
2021-08-24 12:09:21 +0700

    done 69, got 88ms :white_check_mark:

---
+++ leetcode/sqrt-69.ts
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/sqrtx/
+ * Bài này chính xác là tìm số nguyên A sao cho nó bình phương gần bằng hoặc bằng X
+ * Đi tìm số nguyên dương tiệm cận X.
+ */
+function mySqrt(x: number): number {
+	if (x === 0 || x === 1) return x
+	let left = 0,
+		right = x
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2)
+		const sqrt = mid * mid
+		const sqrtTop = (mid + 1) * (mid + 1)
+		const sqrtBot = (mid - 1) * (mid - 1)
+		if (sqrt === x) return mid
+		else if (x < sqrt) {
+			if (x < sqrtBot) right = mid
+			else return mid - 1
+		} else {
+			if (sqrtTop < x) left = mid
+			else return mid
+		}
+	}
+	return -1
+}
+console.log(mySqrt(36))
