8e55454698b3f5a5459416e4856d90c6e1066ae8
Hieu <tranhieu14310@gmail.com>
2021-08-28 11:26:04 +0700

    done 342

---
+++ leetcode/powerOfFour-342.ts
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/power-of-four/
+ *
+ */
+function isPowerOfFour(n: number): boolean {
+	const recursion = (n: number, i: number): boolean => {
+		if (n === 1) return true
+		else if (4 ** i === n) return true
+		else if (4 ** i > n) return false
+		else return recursion(n, i + 1)
+	}
+	return recursion(n, 0)
+}
+console.log(isPowerOfFour(5))
