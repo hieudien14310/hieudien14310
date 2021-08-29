1051da92c2986daab054818cd8709fb601d2a686
Hieu <tranhieu14310@gmail.com>
2021-08-29 17:19:49 +0700

    insert at index

--- Library/linkedList.ts
+++ Library/linkedList.ts
@@ -34,6 +34,32 @@ class LinkedList {
 		}
 		this.size++
 	}
+	// Insert at index
+	insertAt(data: number, index: number) {
+		if (index > 0 && index > this.size) return
+		else if (index === 0) {
+			this.insertFirst(data)
+		} else if (index === this.size) {
+			this.insertLast(data)
+		} else {
+			const node = new Node1(data)
+
+			let current = this.head,
+				previous,
+				count = 0
+			// Tìm khoảng giữa để insert data vào.
+			while (count < index) {
+				previous = current
+				count++
+				current = current.next
+			}
+			// Insert giá trị tại vị trí index
+			previous.next = node
+			node.next = current
+
+			this.size++
+		}
+	}
 	// Print list data
 	printListData() {
 		let current = this.head
@@ -48,5 +74,8 @@ const ll = new LinkedList()
 // ll.insertFirst(200)
 ll.insertFirst(50)
 ll.insertLast(500)
+ll.insertAt(51, 0)
+ll.insertAt(52, 3)
+ll.insertAt(53, 2)
 ll.printListData()
-console.log(ll)
+// console.log('result: ', ll)
