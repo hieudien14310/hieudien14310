37017e2a887f54c328123a0784e5ecfb4793b3bd
Hieu <tranhieu14310@gmail.com>
2021-08-29 18:31:35 +0700

    done linkedlist

--- Library/linkedList.ts
+++ Library/linkedList.ts
@@ -15,12 +15,12 @@ class LinkedList {
 	}
 
 	// Insert first node
-	insertFirst(data: number) {
+	insertFirst(data: number): void {
 		this.head = new Node1(data, this.head)
 		this.size++
 	}
 	// Insert last node
-	insertLast(data: number) {
+	insertLast(data: number): void {
 		let node = new Node1(data)
 		let current
 		if (!this.head) {
@@ -35,7 +35,7 @@ class LinkedList {
 		this.size++
 	}
 	// Insert at index
-	insertAt(data: number, index: number) {
+	insertAt(data: number, index: number): void {
 		if (index > 0 && index > this.size) return
 		else if (index === 0) {
 			this.insertFirst(data)
@@ -60,8 +60,41 @@ class LinkedList {
 			this.size++
 		}
 	}
+	// Get at index
+	getAt(index: number): void {
+		let current = this.head,
+			count = 0
+		while (count < index) {
+			current = current.next
+			count++
+		}
+		console.log('At: ', current.data)
+	}
+	// Remove at index
+	removeAt(index: number): void {
+		if (index > 0 && index >= this.size) return
+		if (index === 0) {
+			this.head = this.head.next
+		} else {
+			let current = this.head,
+				previous,
+				count = 0
+			while (count < index) {
+				previous = current
+				current = current.next
+				count++
+			}
+			previous.next = current.next
+		}
+		this.size--
+	}
+	// Clear linked list
+	clear(): void {
+		this.head = null
+		this.size = 0
+	}
 	// Print list data
-	printListData() {
+	printListData(): void {
 		let current = this.head
 		while (current) {
 			console.log(current.data)
@@ -77,5 +110,8 @@ ll.insertLast(500)
 ll.insertAt(51, 0)
 ll.insertAt(52, 3)
 ll.insertAt(53, 2)
+ll.getAt(1)
+ll.removeAt(5)
+// ll.clear()
 ll.printListData()
 // console.log('result: ', ll)
