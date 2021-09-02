b8507d738f5af0dcc3a45e8e4cc971b3d310ec04
Hieu <tranhieu14310@gmail.com>
2021-09-02 18:37:55 +0700

    done 876, got 76ms :white_check_mark:

---
+++ leetcode/middleOfTheLinkedList-876.ts
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ *
+ */
+class ListNode876 {
+	val: number
+	next: ListNode876 | null
+	constructor(val?: number, next?: ListNode876 | null) {
+		this.val = val === undefined ? 0 : val
+		this.next = next === undefined ? null : next
+	}
+}
+const ll876 = new ListNode876(
+	1,
+	new ListNode876(
+		2,
+		new ListNode876(
+			3,
+			new ListNode876(4, new ListNode876(5, new ListNode876(6, null))),
+		),
+	),
+)
+function middleNode(head: ListNode876 | null): ListNode876 | null {
+	let newHead: ListNode876 | null = head
+	let size = 0
+	while (head !== null) {
+		size++
+		head = head.next
+	}
+	const middle = Math.floor(size / 2)
+	let i = 0
+	while (i < middle) {
+		if (newHead) newHead = newHead.next
+		i++
+	}
+	return newHead
+}
+
+console.log(middleNode(ll876))
