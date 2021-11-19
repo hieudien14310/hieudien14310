be54faf93935b41f46a1ee891e20b1bbb6b74319
hieudien14310 <tranhieu14310@gmail.com>
2021-11-19 17:01:13 +0700

    not good solution

---
+++ Leetcode/Array/countNumberOfPairsK-2006.ts
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
+ * Đếm số cặp i và j sao cho |i - j| === k.
+ * @param nums
+ * @param k
+ */
+function countKDifference(nums: number[], k: number): number {
+	let count = 0
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (Math.abs(nums[i] - nums[j]) === k) count++
+		}
+	}
+	return count
+}
+console.log(`countKDifference`, countKDifference([3, 2, 1, 5, 4], 2))
