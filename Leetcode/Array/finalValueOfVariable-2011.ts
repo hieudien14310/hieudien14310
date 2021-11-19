e9834e970aa0c1e459acd01005ee23601d1a8316
hieudien14310 <tranhieu14310@gmail.com>
2021-11-19 15:57:30 +0700

    solution is valid

---
+++ Leetcode/Array/finalValueOfVariable-2011.ts
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+ * @param operations
+ */
+function finalValueAfterOperations(operations: string[]): number {
+	let X = 0
+	// for (const operation of operations) {
+	// 	if (operation === 'X++' || operation === '++X') X++
+	// 	else X--
+	// }
+	operations.forEach((operation) => eval(operation))
+	return X
+}
+console.log(
+	'finalValueAfterOperations',
+	finalValueAfterOperations(['X++', '++X', '--X', 'X--', 'X++', 'X++']),
+)
