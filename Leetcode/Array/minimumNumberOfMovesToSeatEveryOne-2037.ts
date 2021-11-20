e188580a6fdf094f8766e2d247ebbb61329aa5e2
hieudien14310 <tranhieu14310@gmail.com>
2021-11-20 16:53:25 +0700

    additional note

--- Leetcode/Array/minimumNumberOfMovesToSeatEveryOne-2037.ts
+++ Leetcode/Array/minimumNumberOfMovesToSeatEveryOne-2037.ts
@@ -1,6 +1,18 @@
 /**
  * https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
  * Di chuyển từng học sinh tới từng chiếc ghế sao cho tổng số bước di chuyển là nhỏ nhất.
+ * Cách giải: Sắp xếp lại 2 mảng seats và students theo thứ tự giảm dần để nhận được các cặp số mà
+ * tại các vị trí tương ứng thì chúng ta nhận được học sinh nào gần chiến ghế nào nhất.
+ * Ví dụ: seats: [12, 14, 19, 19, 12], students: [19, 2, 17, 20, 7]
+ * => Sắp xếp giảm dần: seats: [19,19,14,12,12], students: [20, 19, 17, 7, 2]
+ * Các cặp số tương ứng theo từng vị trí của học sinh
+ * Student:   20    19    17     7     2
+ * 			  ||	||	  ||    ||    ||
+ * 			  ||	||	  ||    ||    ||
+ * Moves:     01    00    03    05    10
+ *  		  ||	||	  ||    ||    ||
+ *  		  ||	||	  ||    ||    ||
+ * Seats:     19    19    14    12    12
  * Điều kiện: Số ghế luôn luôn bằng với số học sinh.
  * @param seats
  * @param students
