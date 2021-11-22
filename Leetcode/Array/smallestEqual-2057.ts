b25bae5a7390791e79fbe6680faf4b5f74d45a1c
hieudien14310 <tranhieu14310@gmail.com>
2021-11-22 17:04:27 +0700

    fully great solution

--- Leetcode/Array/smallestEqual-2057.ts
+++ Leetcode/Array/smallestEqual-2057.ts
@@ -6,11 +6,10 @@ function smallestEqual(nums: number[]): number {
 	let smallest: number = Infinity
 	for (let i = 0; i < nums.length; i++) {
 		if (i % 10 === nums[i] && smallest > i) {
-			smallest = i
+			return i
 		}
 	}
-	if (smallest === Infinity) return -1
-	return smallest
+	return -1
 }
 // console.log(`smallestEqual `, smallestEqual([7, 8, 3, 5, 2, 6, 3, 1, 1, 4, 5, 4, 8, 7, 2, 0, 9, 9, 0, 5, 7, 1, 6]))
 console.log(`smallestEqual `, smallestEqual([6, 5, 4, 4, 9, 1, 5, 0, 8, 8, 5, 8, 0, 9, 8, 3, 6, 5, 2, 7, 7, 6, 6, 8, 9, 6, 5, 6, 5, 6, 8, 6, 9, 5, 1, 0, 5, 5]))
